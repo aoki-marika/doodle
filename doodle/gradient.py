c109763fd3a31760103faf83ac1336d3388850ab
marika <marika@waifu.club>
2018-05-08 21:26:27 -0300

    Cleanup.

--- doodle/gradient.py
+++ doodle/gradient.py
@@ -13,13 +13,69 @@ from .drawables import Axes
 
 # todo: midpoints beyond 0.5 are broken
 
-def interpolate(time, startValue, endValue, startTime, endTime, middle):
-    t = (time - startTime) / (endTime - startTime);
-    t = 0.5 * t / middle
+def gradient(percent, startValue, endValue, middle):
+    """
+    Get the gradient value between two colour channels.
 
+    Args:
+        percent (float): The point (from 0 to 1) between `startValue` and
+            `endValue` to get the value of.
+
+        startValue (float): The start value of the interpolation.
+
+        endValue (float): The end value of the interpolation
+
+        middle (float): The middle point of `startValue` between `startValue`
+            and `endValue` (from 0 to 1).
+
+    Returns:
+        float: The value between `startValue` and `endValue` at `percent`,
+            taking into account `middle`.
+    """
+
+    t = 0.5 * (percent / 1) / middle
     return startValue + t * (endValue - startValue);
 
-def draw_gradient(width, height, type, points, axis = Axes.NONE):
+def gradient_tuple(percent, start, end, middle):
+    colour = ()
+
+    for i in range(3):
+        colour += (round(gradient(percent, start[i], end[i], middle)),)
+
+    def alpha(colour):
+        if len(colour) >= 4:
+            return colour[3]
+        else:
+            return 255
+
+    startA = alpha(start)
+    endA = alpha(end)
+    a = round(gradient(percent, startA, endA, middle))
+    colour += (a,)
+
+    return colour
+
+def draw_gradient(width, height, type, points, direction = Axes.NONE):
+    """
+    Render a gradient to an `Image`.
+
+    Args:
+        width (int): The width of the gradient image.
+
+        height (int): The height of the gradient image.
+
+        type (GradientType): The type of this gradient.
+
+        points ([GradientPoint]): An array of `GradientPoint`s used to draw the
+            gradient.
+
+        direction (Axes): The direction of the gradient. X for horizontal and Y
+            for Vertical. Only used for `GradientType.LINEAR`.
+
+    Returns:
+        Image: A gradient made from the given arguments, rendered into an image.
+    """
+
     if len(points) < 2:
         raise ValueError('all gradients must have at least two points')
 
@@ -32,41 +88,37 @@ def draw_gradient(width, height, type, points, axis = Axes.NONE):
     endIndex = 1
 
     if type == GradientType.LINEAR:
-        if axis == Axes.X:
+        if direction == Axes.X:
             distance = width
-        elif axis == Axes.Y:
+        elif direction == Axes.Y:
             distance = height
 
     for i in range(distance):
         start = points[startIndex]
-        after = points[endIndex]
+        end = points[endIndex]
 
         if type == GradientType.LINEAR:
-            if axis == Axes.X:
+            if direction == Axes.X:
                 startPosition = float(width) * start.position
-                endPosition = float(width) * after.position
-            elif axis == Axes.Y:
+                endPosition = float(width) * end.position
+            elif direction == Axes.Y:
                 startPosition = float(height) * start.position
-                endPosition = float(height) * after.position
+                endPosition = float(height) * end.position
 
         percentage = (i - startPosition) / (endPosition - startPosition)
         pointIndex = round(len(points) * percentage)
 
-        r = round(interpolate(percentage, start.colour[0], after.colour[0], 0, 1, start.middle))
-        g = round(interpolate(percentage, start.colour[1], after.colour[1], 0, 1, start.middle))
-        b = round(interpolate(percentage, start.colour[2], after.colour[2], 0, 1, start.middle))
-
-        startA = start.colour[3] if len(start.colour) >= 4 else 255
-        afterA = after.colour[3] if len(after.colour) >= 4 else 255
-        a = round(interpolate(percentage, startA, afterA, 0, 1, start.middle))
-
-        colour = (r, g, b, a)
+        colour = gradient_tuple(percentage, start.colour, end.colour, start.middle)
 
         if type == GradientType.LINEAR:
-            if axis == Axes.X:
-                draw.line([(i, 0), (i, height)], fill=colour)
-            elif axis == Axes.Y:
-                draw.line([(0, i), (width, i)], fill=colour)
+            if direction == Axes.X:
+                start = (i, 0)
+                end = (i, height)
+            elif direction == Axes.Y:
+                start = (0, i)
+                end = (width, i)
+
+            draw.line([start, end], fill=colour)
 
         if i >= endPosition:
             startIndex = min(startIndex + 1, len(points) - 1)
@@ -80,20 +132,19 @@ class GradientType(Enum):
     """
 
     LINEAR = auto()
-    RADIAL = auto()
 
 class GradientPoint:
     """
     A point on a gradient.
 
     Attributes:
-        position (float): The position on the gradient of this point,
-            on a scale of 0 to 1.
+        position (float): The position in the gradient of this point
+            (from 0 to 1).
 
-        colour ((int, int, int)): The RGB tuple colour of this point.
+        colour ((int, int, int)): The RGB(A) tuple colour of this point.
 
         middle (float): The middle point of the colour interpolation
-            between this point and the next in a gradient.
+            between this point and the next in the gradient.
     """
 
     def __init__(self, position, colour, middle = 0.5):
